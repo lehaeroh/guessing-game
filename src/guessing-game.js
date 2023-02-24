8d93937e56648ae8c11b82ac008d0919fcab440c
Aleksey Eroh <alekseyeroh@gmail.com>
2023-02-24 21:47:13 +0300

    feat: resolved task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,55 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.min = null;
+      this.max = null;
+      this.currentGuess = null;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.currentGuess = Math.ceil((this.min + this.max) / 2);
+      return this.currentGuess;
     }
-
+  
     lower() {
-
+      this.max = this.currentGuess;
     }
-
+  
     greater() {
-
+      this.min = this.currentGuess;
     }
-}
+  }
+
+//   const game = new GuessingGame();
+// game.setRange(0, 4048)
+// console.log(game.guess())
+// game.lower();
+// console.log(game.guess())
+// game.lower();
+// console.log(game.guess())
+// game.lower();
+// console.log(game.guess())
+// game.greater();
+// console.log(game.guess())
+// game.greater();
+// console.log(game.guess())
+// game.lower();
+// console.log(game.guess())
+// game.lower();
+// console.log(game.guess())
+// game.greater();
+// console.log(game.guess())
+// game.greater();
+// console.log(game.guess())
+// game.greater();
+// console.log(game.guess())
+// game.lower();
+
+// console.log(game.guess())
 
 module.exports = GuessingGame;
+
